04e51f0697728b33d8b1024657cf1bd376bc699c
AdvaithRakshan <143710607+AdvaithRakshan@users.noreply.github.com>
2023-10-13 10:39:10 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -13,26 +13,26 @@ let allMusic = [
   {
     name: "Harley Bird - Home",
     artist: "Jordan Schor",
-    img: "music-1.jpg",
-    src: "music-1.mp3"
+    img: "music-1",
+    src: "music-1"
   },
   {
     name: "Ikson Anywhere – Ikson",
     artist: "Audio Library",
-    img: "music-2.jpg",
-    src: "music-2.mp3"
+    img: "music-2",
+    src: "music-2"
   },
   {
     name: "Beauz & Jvna - Crazy",
     artist: "Beauz & Jvna",
-    img: "music-3.jpg",
-    src: "music-3.mp3"
+    img: "music-3",
+    src: "music-3"
   },
   {
     name: "Aftershock",
     artist: "Macky Gee",
-    img: "music-4.jpg",
-    src: "music-4.mp3"
+    img: "music-4",
+    src: "music-4"
   },
   
   // like this paste it and remember to give comma after ending of this bracket }
